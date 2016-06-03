123d89ac10404a32203344861dcb00bafcfed698
GeovannyMunoz <geo_roskax100pre@hotmail.com>
2016-06-02 22:51:02 -0500

    Tweets generados de localidades diferentes Norte de la Cuidad

---
+++ tweets1.py
@@ -0,0 +1,45 @@
+
+import couchdb
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+
+
+###API ########################
+ckey = "P92NtY2qIdxUfjsGfivEijmsr"
+csecret = "NBHSV0I3RbocI4hBgI3sZPWTTOfty0riJEqi54wC2Fk301R7Wv"
+atoken = "738207948867014656-u8qyopbKISQcFrUFskwSKoyaE6eL0b1"
+asecret = "IlB2dUi9ViIaRr8tFgBtq7SWDTSLTAdUkgFsDqVsKiBv7"
+#####################################
+
+class listener(StreamListener):
+    
+    def on_data(self, data):
+        dictTweet = json.loads(data)
+        try:
+            dictTweet["_id"] = str(dictTweet['id'])
+            doc = db.save(dictTweet)
+            print "SAVED" + str(doc) +"=>" + str(data)
+        except:
+            print "Already exists"
+            pass
+        return True
+    
+    def on_error(self, status):
+        print status
+        
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, listener())
+
+'''========couchdb'=========='''
+server = couchdb.Server('http://localhost:5984/')  #('http://115.146.93.184:5984/')
+try:
+    db = server.create('tweets1')
+except:
+    db = server['tweets1']
+    
+'''===============LOCATIONS=============='''    
+
+twitterStream.filter(locations=[-78.488989,-0.150375,-78.457403,-0.125313])  #6 de diceimbre y eloy alfaro
